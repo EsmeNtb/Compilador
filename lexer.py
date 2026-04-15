875168553b263b79cb80ade9f3aa4d9f2b0b36b9
Kenia Esmeralda Ramos Javier <esme.E_N.tb@outlook.com>
2026-04-15 06:34:26 -0600

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,11 +1,12 @@
 from globalTypes import *
 
+# Clasificación de Caracteres de entrada para el DFA
 LETRA = 0
 DIGITO = 1
 SIMBOL = 2
 DELIM = 3
 OTRO = 4
-# Posibles entradas
+# Posibles entradas 
 
 """ 
     ESTADOS DE DFA
@@ -17,8 +18,13 @@ OTRO = 4
 """
 
 tabla = [ 
-#     Tabla basada en mi DFA
-#   Letra  Digito Simbol Delim      OTRO
+# -------------------------------------------------------
+# TABLA DE TRANSICIONES DEL DFA
+# Filas = estados actuales
+# Columnas = tipo de carácter de entrada
+#
+#           Letra  Digito  Simbol  Delim  Otro
+# -------------------------------------------------------
     [1,     2,      4,      0,      3], #qo
     [1,     3,      4,      4,      3], #q1
     [3,     2,      4,      4,      3], #q2
@@ -28,6 +34,15 @@ tabla = [
 
 
 def globales(prog, pos, long):
+    """
+       Inicializa las variables globales del analizador léxico.
+
+    Parámetros:
+        prog : string completo del programa fuente
+        pos  : posición inicial de lectura
+        long : longitud del programa
+    """
+
     global programa, posicion, progLong
 
     programa = prog
@@ -36,12 +51,31 @@ def globales(prog, pos, long):
 
 
 def reservedLookup(tokenString):
+    """
+        Revisa si el lexema recibido es una palabra reservada.
+
+        Si coincide con alguna palabra en ReservedWords,
+        regresa el TokenType correspondiente.
+        En caso contrario, se clasifica como identificador (ID).
+    """
+
     for w in ReservedWords:
         if tokenString == w.value:
             return TokenType[w.name]
     return TokenType.ID
 
 def tipo_char(c):
+    """
+    Clasifica un carácter en una de las categorías del DFA.
+
+    Retorna:
+        LETRA   si es letra
+        DIGITO  si es número
+        SIMBOL  si es símbolo reconocido
+        DELIM   si es espacio, tab o salto de línea
+        OTRO    cualquier otro carácter no válido
+    """
+    
     if c.isalpha():
         return LETRA
     elif c.isdigit():
@@ -54,6 +88,15 @@ def tipo_char(c):
         return OTRO
 
 def getSymbolToken(c):
+    """
+    Convierte un símbolo individual en su token correspondiente.
+
+    Ejemplo:
+        '+' -> TokenType.PLUS
+        '(' -> TokenType.LPAREN
+    Si el símbolo no existe en el mapa, devuelve ERROR.
+    """
+
     mapa = {
         '+': TokenType.PLUS,
         '-': TokenType.MINUS,
@@ -76,9 +119,24 @@ def getSymbolToken(c):
 
 # Detector de la posición del error
 def line_colum_error(programa,pos):
+    """
+    Calcula la línea, el texto de la línea y la columna
+    donde ocurrió un error léxico.
+
+    Parámetros:
+        programa : código fuente completo
+        pos      : posición absoluta del error
+
+    Retorna:
+        line_num : número de línea
+        tex_line : contenido completo de esa línea
+        colum    : columna exacta del error
+    """
+
     line_num = 1
     initial_line = 0
 
+    # Recorre el programa hasta la posición del error
     for i in range (pos):
         if programa[i] == '\n':
             line_num += 1
@@ -96,6 +154,11 @@ def line_colum_error(programa,pos):
 
 # Print the error
 def printError(tipeError, pos):
+    """
+    Imprime un mensaje de error con formato visual,
+    indicando la línea y marcando con '^' el punto exacto.
+    """
+     
     global programa
     line_num, tex_line, colum = line_colum_error(programa,pos)
     
@@ -104,12 +167,24 @@ def printError(tipeError, pos):
     print(' ' * colum + '^')
 
 
+# Función principal del Analizador Léxico
 def getToken(imprime = True):
+    """
+    Obtiene el siguiente token del programa fuente.
+
+    Parámetro:
+        imprime : si es True, imprime el token encontrado
+
+    Retorna:
+        (token, tokenString)
+    """
+
     global programa, posicion, progLong 
 
     while posicion < len(programa):
         c = programa[posicion]
 
+        # Fin del archivo
         if c == '$':
             token = TokenType.ENDFILE
             tokenString= '$'
@@ -118,10 +193,12 @@ def getToken(imprime = True):
                 print(token, '=', tokenString)
             return token,tokenString
         
+        # Ignora los delimitadores
         elif c in ' \t\n':
             posicion += 1
             continue
-
+        
+         # 3. OPERADORES DE DOS CARACTERES
         elif programa[posicion:posicion+2] == '==':
             token = TokenType.EQ
             tokenString = '=='
@@ -158,6 +235,7 @@ def getToken(imprime = True):
                 print(token, '=', tokenString)
             return token, tokenString
         
+        # 4. MANEJO DE COMENTARIOS BLOQUE: /* ... */
         elif programa[posicion:posicion+2] == '/*':
             ini_comment = posicion
             posicion += 2
@@ -178,7 +256,7 @@ def getToken(imprime = True):
             posicion += 2
             continue
 
-
+        # Procesamiento con el DFA
         estado = 0
         tokenString = "" # String for storing token
         start = posicion
@@ -220,6 +298,7 @@ def getToken(imprime = True):
                 
                 return token, tokenString
 
+            # Estado de error léxicco
             elif nuevoEstado == 3:
                 if estado == 0:
                     tokenString = c
@@ -240,7 +319,7 @@ def getToken(imprime = True):
                         printError("en la formación del token", start)
                 
                 return token,tokenString
-            
+            # Cualquier otro caso inesperado
             else:
                 token = TokenType.ERROR
 
